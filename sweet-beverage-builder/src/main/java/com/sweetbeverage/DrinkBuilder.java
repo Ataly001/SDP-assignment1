b659b9cc52a47b4b3eb9de2dfe8cb272a5d3d365
Ataly001 <ataly9808@gmail.com>
2026-09-10 22:35:39 +0500

    corrected structure

--- sweet-beverage-builder/src/main/java/com/sweetbeverage/DrinkBuilder.java
+++ sweet-beverage-builder/src/main/java/com/sweetbeverage/DrinkBuilder.java
@@ -3,7 +3,8 @@ package com.sweetbeverage;
 public interface DrinkBuilder {
     DrinkBuilder setName(String name);
     DrinkBuilder setFlavor(String flavor);
-    DrinkBuilder setSugarGrams(boolean carbonated);
+    DrinkBuilder setSugarGrams(int sugarGrams);
+    DrinkBuilder setCarbonated(boolean carbonated);
     DrinkBuilder setCaffeineMg(int caffeineMg);
     DrinkBuilder setVolumeMl(int volumeMl);
     DrinkBuilder setMainIngredient(String mainIngredient);
